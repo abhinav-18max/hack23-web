4bd629790d167a39fde6def11890c0bf02961601
unknown <voa.alan@gmail.com>
2023-08-05 23:28:23 +0530

    'Slider'

--- assets/js/main.js
+++ assets/js/main.js
@@ -235,21 +235,45 @@
   /**
    * Sponosors slider
    */
-  new Swiper('.sponsors-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  function initSwiperSliders() {
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const swiperContainers = document.querySelectorAll('.sponsors-slider');
 
+    swiperContainers.forEach((swiperContainer) => {
+      const swiperId = swiperContainer.dataset.swiperId;
+
+      if (screenWidth <= 430) {
+        if (!swiperContainer[swiperId]) {
+          swiperContainer[swiperId] = new Swiper(swiperContainer, {
+            // Swiper configuration options for smaller screens
+            slidesPerView: 'auto',
+            spaceBetween: 20,
+            loop: true,
+            autoplay: {
+              delay: 3000,
+              disableOnInteraction: false,
+            },
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'bullets',
+              clickable: true,
+            },
+          });
+        }
+      } else {
+        if (swiperContainer[swiperId]) {
+          swiperContainer[swiperId].destroy();
+          swiperContainer[swiperId] = null;
+        }
+      }
+    });
+  }
+
+  // Event listener for page load and window resize
+  window.addEventListener('load', () => {
+    initSwiperSliders();
+  });
+  window.addEventListener('resize', initSwiperSliders);
   /**
    * Animation on scroll
    */
